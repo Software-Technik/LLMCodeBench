be6ae6cc4264092b4563cc07c71ebaa23e15f40c
Stelle Djanto <stelle.djamgadjanto@stud.hs-ruhrwest.de>
2025-06-16 00:01:55 +0200

    solve merge conflict

---
+++ project_root/2016/6/human.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+inout_strings = sys.argv[1]
+
+with open(inout_strings, 'r') as infile:
+    noise = infile.readlines()
+
+columns = (''.join(column) for column in zip(*noise))
+
+first_solution = ''
+second_solution = ''
+
+for column in columns:
+    (most, _), *others, (least, _) = Counter(column).most_common()
+    first_solution += most
+    second_solution += least
+
+print("The message usually consists of the most frequent letters....")
+print("Then it must be:", first_solution)
+print("....")
+print("Or is it the least frequent letters? I never know....")
+print("It might be then:", second_solution)
