7c2114cbafb7ea6c529f91c28689392199826408
Andreas Seeliger <seeliger.andreas@web.de>
2025-07-02 21:12:09 +0200

    removed all human.py files taht did work partioally or not at all

--- project_root/2018/21/aoc2018-day21.py
+++
@@ -1,8 +0,0 @@
-import register
-
-
-reg_op = register.RegisterOperations()
-reg_op.load_program("day21-input.txt")
-
-print(f"Answer part  I: {reg_op.run_program_one()}")
-print(f"Answer part II: {reg_op.run_program_two()}")
